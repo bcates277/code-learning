00e3c8904dff285d4c634ab41ff7705051c14a79
Betsy Cates <betsycates@betsycates-macbook-air.tailcb7ae.ts.net>
2025-05-07 14:13:25 -0400

    race-day.js

---
+++ javascript/race-day.js
@@ -0,0 +1,13 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+const registeredEarly = true;
+const runnersAge = 18;
+
+if (runnersAge > 18 && registeredEarly) {
+  raceNumber += 1000;
+  console.log(`You will be racing at 9:30AM. Your race number is lucky number ${raceNumber}!`);
+} else if (runnersAge > 18 && !registeredEarly) {
+  console.log(`You will race at 11:00AM and your lucky race number will be ${raceNumber}!`);
+} else if (runnersAge < 18) {
+  console.log (`You will race at 12:30PM and your lucky race number will be ${raceNumber}!`);
+} else console.log(`Please see the registration desk!`);
+
