32767028b9dee7f332c87c344950de720d5ae220
Betsy Cates <betsycates@betsycates-macbook-air.tailcb7ae.ts.net>
2025-05-07 22:46:16 -0400

    rock-paper-scissors.js

--- javascript/magic-eight-ball.js
+++ javascript/magic-eight-ball.js
@@ -1,55 +1,55 @@
 function getEightBallAnswer() {
-    // randomNumber is a variable that will hold a random number between 0 and 7
-    // Math.random() generates a random number between 0 and 1
-    // Multiplying by 8 gives us a number between 0 and 8
-    // Math.floor() rounds down to the nearest whole number
-    // So randomNumber will be a number between 0 and 7
-    let randomNumber = Math.floor(Math.random() * 8);
-    
-    // eightBallAnswer is a variable that will hold the answer
-    let eightBallAnswer = '';
+  // randomNumber is a variable that will hold a random number between 0 and 7
+  // Math.random() generates a random number between 0 and 1
+  // Multiplying by 8 gives us a number between 0 and 8
+  // Math.floor() rounds down to the nearest whole number
+  // So randomNumber will be a number between 0 and 7
+  let randomNumber = Math.floor(Math.random() * 8);
 
-    switch (randomNumber) {
-        case 0:
-            eightBallAnswer = 'It is certain';
-            break;
-        case 1:
-            eightBallAnswer = 'It is decidedly so';
-            break;
-        case 2:
-            eightBallAnswer = 'Cannot predict now';
-            break;
-        case 3:
-            eightBallAnswer = 'Reply hazy try again';
-            break;
-        case 4:
-            eightBallAnswer = 'Do not count on it';
-            break;
-        case 5:
-            eightBallAnswer = 'My sources say no';
-            break;
-        case 6:
-            eightBallAnswer = 'Outlook not so good';
-            break;
-        case 7:
-            eightBallAnswer = 'Signs point to yes';
-            break;
-        default:
-            eightBallAnswer = 'No answer...';
-            break;
-    }
+  // eightBallAnswer is a variable that will hold the answer
+  let eightBallAnswer = "";
 
-    return eightBallAnswer;
+  switch (randomNumber) {
+    case 0:
+      eightBallAnswer = "It is certain";
+      break;
+    case 1:
+      eightBallAnswer = "It is decidedly so";
+      break;
+    case 2:
+      eightBallAnswer = "Cannot predict now";
+      break;
+    case 3:
+      eightBallAnswer = "Reply hazy try again";
+      break;
+    case 4:
+      eightBallAnswer = "Do not count on it";
+      break;
+    case 5:
+      eightBallAnswer = "My sources say no";
+      break;
+    case 6:
+      eightBallAnswer = "Outlook not so good";
+      break;
+    case 7:
+      eightBallAnswer = "Signs point to yes";
+      break;
+    default:
+      eightBallAnswer = "No answer...";
+      break;
+  }
+
+  return eightBallAnswer;
 }
 
 // Example usage:
-const userName = 'Betsy';
-const userQuestion = 'Should I get another cat?';
+const userName = "Betsy";
+const userQuestion = "Should I get another cat?";
 
 if (userName) {
-    console.log(`Hello ${userName}`);
+  console.log(`Hello ${userName}`);
 } else {
-    console.log('Hello');
+  console.log("Hello");
 }
 
 console.log(`${userName} wants to ask: ${userQuestion}`);
--- javascript/race-day.js
+++ javascript/race-day.js
@@ -4,10 +4,15 @@ const runnersAge = 18;
 
 if (runnersAge > 18 && registeredEarly) {
   raceNumber += 1000;
-  console.log(`You will be racing at 9:30AM. Your race number is lucky number ${raceNumber}!`);
+  console.log(
+    `You will be racing at 9:30AM. Your race number is lucky number ${raceNumber}!`
+  );
 } else if (runnersAge > 18 && !registeredEarly) {
-  console.log(`You will race at 11:00AM and your lucky race number will be ${raceNumber}!`);
+  console.log(
+    `You will race at 11:00AM and your lucky race number will be ${raceNumber}!`
+  );
 } else if (runnersAge < 18) {
-  console.log (`You will race at 12:30PM and your lucky race number will be ${raceNumber}!`);
+  console.log(
+    `You will race at 12:30PM and your lucky race number will be ${raceNumber}!`
+  );
 } else console.log(`Please see the registration desk!`);
-
